b2dfb890f761dc7918a6b22ea20b736674d2f4df
kiefersv <reronex@gmail.com>
2026-07-27 17:31:01 -0500

    correct testing workflow

--- nimbus/src/spectra.py
+++ nimbus/src/spectra.py
@@ -50,7 +50,7 @@ def picaso_formater(self, tag=None, ds_prev=None, path_to_opacities=None, sig=2,
     if mieai_object is not None:
         ma = mieai_object
     else:
-        from mieai import MieNet
+        from mienet import MieNet
         ma = MieNet(use_ai=False)  # set up mieai class
 
     # ==== Set optional inputs
